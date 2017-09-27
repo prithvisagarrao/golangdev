645b8d4d34e5822ae2666a1ff227e7f928628773
Prithvisagar Rao <prithvisagarrao@gmail.com>
2017-09-27 13:48:51 +0000

    concurrency

---
+++ mutex.go
@@ -0,0 +1,37 @@
+package main
+
+import(
+	_ "time"
+	"fmt"
+	"sync"
+)
+
+type SafeCounter struct{
+	v map[string]int
+	mux sync.Mutex
+}
+
+func (c *SafeCounter) Inc(key string) {
+	c.mux.Lock()
+	c.v[key] ++
+	c.mux.Unlock()
+}
+
+func (c *SafeCounter) Value(key string) int{
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
+func main() {
+
+	c := SafeCounter{v: make(map[string]int)}
+	for i := 0; i<43; i++{
+		go c.Inc("somekey")
+	}
+	
+	
+	var inpo string
+	fmt.Scanln(&inpo)
+	fmt.Println(c.Value("somekey"))
+}
